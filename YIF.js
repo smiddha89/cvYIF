b509b59c467952ad02b8ea4fb6e6fb8b5cb8c40c
smiddha89 <83001792+smiddha89@users.noreply.github.com>
2021-04-22 13:35:59 +0530

    Add files via upload

---
+++ YIF.js
@@ -0,0 +1,19 @@
+function openForm() {
+  document.getElementById("myForm1").style.display = "block";
+}
+function openForm() {
+  document.getElementById("myForm2").style.display = "block";
+}
+function openForm() {
+  document.getElementById("myForm3").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm1").style.display = "none";
+}
+function closeForm() {
+  document.getElementById("myForm2").style.display = "none";
+}
+function closeForm() {
+  document.getElementById("myForm3").style.display = "none";
+}
